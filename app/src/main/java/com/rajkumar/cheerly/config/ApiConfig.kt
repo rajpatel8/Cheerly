2fc5563d712ed0dd6a489b3009444c7ce5d5e652
Lord Rajkumar <rajphx299@gmail.com>
2024-12-08 18:33:29 -0500

    Update ApiConfig.kt
    Signed-off-by: Lord Rajkumar <rajphx299@gmail.com>

--- app/src/main/java/com/rajkumar/cheerly/config/ApiConfig.kt
+++ app/src/main/java/com/rajkumar/cheerly/config/ApiConfig.kt
@@ -3,7 +3,7 @@ package com.rajkumar.cheerly.config
 object ApiConfig {
     // YouTube Data API v3 configuration
     const val YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3/"
-    const val YOUTUBE_API_KEY = "AIzaSyC_xEkXM8LJz0OvMmQLXP5eF1OTIM1tj4w"  // Keep as fallback
+    const val YOUTUBE_API_KEY = ""  // Keep as fallback
 
     // API request parameters
     const val DEFAULT_REGION_CODE = "IN"  // India
