2355af3b367221c1f8c1a96239809302c44292e9
Lord Rajkumar <rajphx299@gmail.com>
2024-12-08 18:34:09 -0500

    Update ApiKeys.kt
    Signed-off-by: Lord Rajkumar <rajphx299@gmail.com>

--- app/src/main/java/com/rajkumar/cheerly/config/ApiKeys.kt
+++ app/src/main/java/com/rajkumar/cheerly/config/ApiKeys.kt
@@ -1,6 +1,6 @@
 package com.rajkumar.cheerly.config
 
 object ApiKeys {
-    const val TICKETMASTER_API_KEY = "iZUHRae7dAFrpiI5Tx0CZxeEv7zxRQBd"
-    const val OPENWEATHER_API_KEY = "d9815615d61b2c2af616326ee57bf08d"
+    const val TICKETMASTER_API_KEY = ""
+    const val OPENWEATHER_API_KEY = ""
 }
